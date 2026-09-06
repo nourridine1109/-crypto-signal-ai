3a103dbee3f73ca42edcb98829bab6881f7b1faf
nourridine1109 <6zdwdcrm8m@privaterelay.appleid.com>
2026-09-06 08:10:01 +0200

    Update app.py
    verbesserung2

--- app.py
+++ app.py
@@ -253,7 +253,7 @@ def get_stock_news(symbol):
             text = f"{title} {summary}".lower()
 
 # Nur News behalten, bei denen die Aktie direkt im Titel vorkommt
-if not any(term in title_text for term in relevant_terms):
+            if not any(term in title_text for term in relevant_terms):
     continue
             continue
             title_text = title.lower()
